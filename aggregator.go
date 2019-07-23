5931284e07260e251ef758ad60aaa46c9bd7110c
Evgenii <evgeniy@nspcc.ru>
2019-07-23 11:15:09 +0300

    Add mean price aggregator for IQR
    Mean price value involving only interquartile range can
    be useful as a way to deal with outliers.

--- aggregator.go
+++ aggregator.go
@@ -1,5 +1,9 @@
 package netmap
 
+import (
+	"sort"
+)
+
 type (
 	// Aggregator can calculate some value across all netmap
 	// such as median, minimum or maximum.
@@ -27,6 +31,11 @@ type (
 		min uint64
 	}
 
+	meanPriceIQRAgg struct {
+		k   float64
+		arr []uint64
+	}
+
 	reverseMinNorm struct {
 		min float64
 	}
@@ -47,6 +56,7 @@ var (
 	_ Aggregator = (*meanCapSumAgg)(nil)
 	_ Aggregator = (*meanCapAgg)(nil)
 	_ Aggregator = (*minPriceAgg)(nil)
+	_ Aggregator = (*meanPriceIQRAgg)(nil)
 
 	_ Normalizer = (*reverseMinNorm)(nil)
 	_ Normalizer = (*sigmoidNorm)(nil)
@@ -109,6 +119,39 @@ func (a *minPriceAgg) Add(n Node) {
 
 func (a *minPriceAgg) Compute() float64 {
 	return float64(a.min)
+}
+
+func (a *meanPriceIQRAgg) Add(n Node) {
+	a.arr = append(a.arr, n.P)
+}
+
+func (a *meanPriceIQRAgg) Compute() float64 {
+	l := len(a.arr)
+	if l == 0 {
+		return 0
+	}
+
+	sort.Slice(a.arr, func(i, j int) bool { return a.arr[i] < a.arr[j] })
+
+	var min, max float64
+	if l < 4 {
+		min, max = float64(a.arr[0]), float64(a.arr[l-1])
+	} else {
+		start, end := l/4, l*3/4-1
+		iqr := a.k * float64(a.arr[end]-a.arr[start])
+		min, max = float64(a.arr[start])-iqr, float64(a.arr[end])+iqr
+	}
+
+	count := 0
+	sum := float64(0)
+	for _, e := range a.arr {
+		t := float64(e)
+		if t >= min && t <= max {
+			sum += float64(t)
+			count++
+		}
+	}
+	return sum / float64(count)
 }
 
 func (r *reverseMinNorm) Normalize(w float64) float64 {
